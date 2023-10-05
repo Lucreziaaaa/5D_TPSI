c9d34417d42b72d2ab7fefe69109a1965d6cd9a8
lucrezia melchionda <l.melchionda.3374@vallauri.edu>
2023-10-05 09:41:50 +0200

    primo commit

---
+++ ANGULAR/intro/src/app/intest/intest.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'intest', //tag da mettere sulla pah app.component.html
+  templateUrl: './intest.component.html',
+  styleUrls: ['./intest.component.css']
+})
+export class IntestComponent {
+
+}
